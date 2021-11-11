7e77d211879a7ddcbca0c3b780ca2e7e11e5c102
Nazar Tytskyi <nazartitskiy@gmail.com>
2021-11-11 16:06:28 +0200

    Update isValueExistInTree.js

--- isValueExistInTree.js
+++ isValueExistInTree.js
@@ -1,6 +1,18 @@
+// Solution 1
+
 const isValueExistInTree = (arr, number) => {
   const searchValue = `[${number},`
   const arrJSON = JSON.stringify(arr);
 
   return arrJSON.includes(searchValue);
+}
+
+
+// Solution 2 (The same idea as in first solution but with RegExp usage)
+
+const isValueExistInTree = (arr, number) => {
+  const regExp = new RegExp(`\\[${number},`)
+  const arrJSON = JSON.stringify(arr);
+
+  return regExp.test(arrJSON);
 }
